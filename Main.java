83326f70cae5e36db7a7d5887dc38f15565b7450
Datta <avachardatta2611@gmail.com>
2024-04-30 15:07:31 +0530

    New File add for java

--- Main.java
+++ Main.java
@@ -4,8 +4,14 @@ public class Main
 
     public static void main(String[] args) {
         
-        System.out.println("Hello");
-
+        System.out.println("Hello World");
+        System.out.println("I Am Learning Java");
+        System.out.println("It is Awesome");
+        System.out.println(3);
+        System.out.println(358);
+        System.out.println(54654654);
+        System.out.println(3+5);
+        System.out.println(2*4);
 
 
     }
