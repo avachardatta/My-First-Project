263e67f4ced08d8e193b8a4ece2a5bee0a778ba7
Datta <avachardatta2611@gmail.com>
2024-05-14 11:05:23 +0530

    New file add

---
+++ abc.java
@@ -0,0 +1,42 @@
+
+
+public class abc {
+
+    
+
+    public static void main(String[] args) {
+        
+    
+        //    byte; ----------- 1 byte
+        //    boolean;----------1 byte
+        //    char;-------------2 byte
+        //    short;------------2 byte
+        //    int;--------------4 byte
+        //    float;------------4 byte
+        //    long;-------------8 byte
+        //    double;-----------8 byte
+        
+
+            int a = 15;
+            System.out.println("Int Value : " + a);
+
+            float b = a;
+            System.out.println("Float Value : " + b);
+
+            double c = b;
+            System.out.println("Long Value " + c);
+
+
+
+
+
+
+        
+
+
+
+
+
+    }    
+}
+
