90ab15952fc2fe2df71dc47d65413fe99db1d955
Datta <avachardatta2611@gmail.com>
2024-05-22 10:42:57 +0530

    File edited, new project

--- abc.java
+++ abc.java
@@ -1,4 +1,4 @@
-
+import java.util.Scanner;
 
 public class abc {
 
@@ -6,16 +6,69 @@ public class abc {
 
     public static void main(String[] args) {
 
-        int arr [];
-        arr = new int [2];
-        arr[0] = 0;
-        arr[1] = 1;
-        // arr[2] = 20;
+        // Data Type
+        
+        byte a = 10;
+        boolean b = true;
+        char c = 'A';
+        short d = 5000;
+        int e = 500000;
+        float f = 50000.500f;
+        long h = 506516561321l;
+        double i = 512.512;
 
-            System.out.println(arr);
-           System.out.println(arr[0]);
+
+        System.out.println("Byte : " + a);
+        System.out.println("Boolean : " + b);
+        System.out.println("Char : " + c);
+        System.out.println("Short : " + d);
+        System.out.println("Int : " + e);
+        System.out.println("Float : " + f);
+        System.out.println("Long : " + h);
+        System.out.println("Double : " + i);
 
 
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter 1st Number");
+        double num1 = sc.nextDouble();
+        System.out.println("Enter Operator (+ ,-,*,/) : ");
+        char operator = sc.next().charAt(0);
+        System.out.println("Enter 2nd Number");
+        double num2 = sc.nextDouble();
+
+        // calculator
+
+        double result;
+        switch (operator) {
+            case '+':
+                result = num1 + num2;
+                break;
+        
+            case '-':
+                result = num1 - num2;
+                break;
+            
+            case '*':
+                result = num1 * num2;
+                break;
+        
+            case '/':
+            if(num2 == 0)
+            {
+                System.out.println("Can Not Divide by zero");
+                return;
+            }
+            result = num1 / num2;
+            break;
+
+            default :
+            System.out.println("Enter valid operator");
+            return;
+
+        }
+
+        System.out.println("Result : " + num1 + " " +  operator + " " +  num2 + " = " + result);
 
     }    
 }
