c8f1a22683305dd4895689cb34a48dc52da0b755
Datta <avachardatta2611@gmail.com>
2024-05-22 10:44:19 +0530

    hide the programme

--- abc.java
+++ abc.java
@@ -1,4 +1,4 @@
-import java.util.Scanner;
+// import java.util.Scanner;
 
 public class abc {
 
@@ -8,67 +8,67 @@ public class abc {
 
         // Data Type
         
-        byte a = 10;
-        boolean b = true;
-        char c = 'A';
-        short d = 5000;
-        int e = 500000;
-        float f = 50000.500f;
-        long h = 506516561321l;
-        double i = 512.512;
+        // byte a = 10;
+        // boolean b = true;
+        // char c = 'A';
+        // short d = 5000;
+        // int e = 500000;
+        // float f = 50000.500f;
+        // long h = 506516561321l;
+        // double i = 512.512;
 
 
-        System.out.println("Byte : " + a);
-        System.out.println("Boolean : " + b);
-        System.out.println("Char : " + c);
-        System.out.println("Short : " + d);
-        System.out.println("Int : " + e);
-        System.out.println("Float : " + f);
-        System.out.println("Long : " + h);
-        System.out.println("Double : " + i);
-
-
-
-        Scanner sc = new Scanner(System.in);
-        System.out.println("Enter 1st Number");
-        double num1 = sc.nextDouble();
-        System.out.println("Enter Operator (+ ,-,*,/) : ");
-        char operator = sc.next().charAt(0);
-        System.out.println("Enter 2nd Number");
-        double num2 = sc.nextDouble();
+        // System.out.println("Byte : " + a);
+        // System.out.println("Boolean : " + b);
+        // System.out.println("Char : " + c);
+        // System.out.println("Short : " + d);
+        // System.out.println("Int : " + e);
+        // System.out.println("Float : " + f);
+        // System.out.println("Long : " + h);
+        // System.out.println("Double : " + i);
 
         // calculator
 
-        double result;
-        switch (operator) {
-            case '+':
-                result = num1 + num2;
-                break;
+
+        // Scanner sc = new Scanner(System.in);
+        // System.out.println("Enter 1st Number");
+        // double num1 = sc.nextDouble();
+        // System.out.println("Enter Operator (+ ,-,*,/) : ");
+        // char operator = sc.next().charAt(0);
+        // System.out.println("Enter 2nd Number");
+        // double num2 = sc.nextDouble();
+
+
+        // double result;
+        // switch (operator) {
+        //     case '+':
+        //         result = num1 + num2;
+        //         break;
         
-            case '-':
-                result = num1 - num2;
-                break;
+        //     case '-':
+        //         result = num1 - num2;
+        //         break;
             
-            case '*':
-                result = num1 * num2;
-                break;
+        //     case '*':
+        //         result = num1 * num2;
+        //         break;
         
-            case '/':
-            if(num2 == 0)
-            {
-                System.out.println("Can Not Divide by zero");
-                return;
-            }
-            result = num1 / num2;
-            break;
+        //     case '/':
+        //     if(num2 == 0)
+        //     {
+        //         System.out.println("Can Not Divide by zero");
+        //         return;
+        //     }
+        //     result = num1 / num2;
+        //     break;
 
-            default :
-            System.out.println("Enter valid operator");
-            return;
+        //     default :
+        //     System.out.println("Enter valid operator");
+        //     return;
 
-        }
+        // }
 
-        System.out.println("Result : " + num1 + " " +  operator + " " +  num2 + " = " + result);
+        // System.out.println("Result : " + num1 + " " +  operator + " " +  num2 + " = " + result);
 
     }    
 }
